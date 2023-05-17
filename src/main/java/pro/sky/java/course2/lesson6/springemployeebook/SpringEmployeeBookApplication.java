fe3448365a274e11779dc8954c27eaf6fc3f05b5
eduard <eduard42rus@mail.ru>
2023-05-17 21:17:41 +0700

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/lesson6/springemployeebook/SpringEmployeeBookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lesson6.springemployeebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEmployeeBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/lesson6/springemployeebook/SpringEmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.lesson6.springemployeebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
